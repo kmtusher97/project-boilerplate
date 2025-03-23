c54667e41b3dc39eb8b88bf41a6ee186ea2dd94b
kmtusher97 <kmtusher97@gmail.com>
2025-03-23 15:37:08 +0600

    Add env files for client

--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authServices from '../services/authServices';
 
 const Axios = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
